cc694ff7e2b717f2ae6bff873a25e63cae355f36
Kacelo <vernonkachelo@gmail.com>
2025-01-13 15:30:38 +0200

    added new Process form

--- src/components/ui/process/form/process-form.tsx
+++ src/components/ui/process/form/process-form.tsx
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 import React from "react";
 import BasicDetailForm from "./basic-detail-form";
 import { User as FirebaseUser } from "firebase/auth";
+import NewProcess from "./new-process/new-process-form";
 
 interface ProcessFormProps {
   user: FirebaseUser;
 }
 
 function ProcessForm(props: ProcessFormProps) {
- const { user } = props;
+  const { user } = props;
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <BasicDetailForm userId={user?.uid}/>
+    <div className="flex w-full items-center justify-center p-6 md:p-10">
+      {/* <BasicDetailForm userId={user?.uid} /> */}
+      <NewProcess userId={user?.uid} />
     </div>
   );
 }
