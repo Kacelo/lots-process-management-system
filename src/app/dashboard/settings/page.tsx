25d13f77ee4f111fb266c9d2d89acf0a12191fbb
Kacelo <vernonkachelo@gmail.com>
2025-02-18 11:43:18 +0200

    change made to setting props interface

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,11 +1,7 @@
 import ProfileForm from "@/components/forms/profile-form";
 import React from "react";
 
-interface settingProps {
-  settings: string;
-}
-
-const Settings = (props: settingProps) => {
+const Settings = () => {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
