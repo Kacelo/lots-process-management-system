7b5c326403639c8b1cefc7405ae97fa6a984c28c
Kacelo <vernonkachelo@gmail.com>
2025-02-21 10:25:59 +0200

    doing this for deployment

--- src/components/ui/process-review/display-assignees.tsx
+++ src/components/ui/process-review/display-assignees.tsx
@@ -15,7 +15,6 @@ function AssigneeAvatar({ assignees }: AssigneeAvaterProps) {
     <div className="flex pt-3">
         <div>
         <p className="mt-[10px] mr-[10px]">Assigned to:</p>
-
         </div>
       {assignees.map((assignee, index) => (
         <div className="cursor-pointer" key={index}>
